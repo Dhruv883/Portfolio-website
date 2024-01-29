26229f6c60e50cf8a5e3c583362e7fc509304510
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-01-29 22:09:59 +0530

    Resume Update

--- app/resume/page.js
+++ app/resume/page.js
@@ -9,7 +9,7 @@ const page = () => {
       <Navbar />
       <div className="h-screen flex items-center justify-center px-3 md:px-10 pt-28">
         <iframe
-          src="https://drive.google.com/file/d/1uP5LzXzVgA_NpYSYIxXWN6vlXMCTlAtj/preview"
+          src="https://drive.google.com/file/d/1H_3FLlCjo7lSJr-vTmVN00OGI5FOKPsy/preview"
           allow="autoplay"
           allowtransparency="true"
           className="w-screen md:w-2/3 h-full bg-darkPurple border-none outline-none"
